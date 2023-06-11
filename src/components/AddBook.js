9c3fc528d57bcc3a589f26e7377b378f1cbd01ce
mahendra7862 <mahendra7862p@gmail.com>
2023-06-11 11:19:40 +0530

    intial commit

---
+++ src/components/AddBook.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default function AddBook(props) {
+
+  return (
+    <div>
+      <div className="add_book">
+                <br />
+                <form onSubmit={props.onSubmit} action=''>
+                    <div className="mb-3">
+                        <label className="form-label">Book Title</label>
+                        <input type="text" className="form-control" value={props.book_name} onChange={(e) => props.setBook_name(e.target.value)} />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Author Name</label>
+                        <input type="text" className="form-control" value={props.author_name} onChange={(e) => props.setAuthor_name(e.target.value)} />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Book Type</label>
+                        <input type="text" className="form-control" value={props.book_type} onChange={(e) => props.setBook_type(e.target.value)} />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Number Of Pages</label>
+                        <input type="number" className="form-control" value={props.no_of_pages} onChange={(e) => props.setNo_of_pages(e.target.value)} />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Ratings</label>
+                        <input type="number" className="form-control" value={props.ratings} onChange={(e) => props.setRatings(e.target.value)} />
+                    </div>
+                    <div className="mb-3">
+                        <input type="submit" className="btn btn-success" value="Add Book" id='add' />
+                        <input type="button" className="btn btn-success" value="Update Book" id='update' style={{ 'display': 'none' }} onClick={() => { props.updateData() }} />
+                    </div>
+                </form>
+            </div>
+    </div>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <nav className="navbar bg-light">
+      <div className="container">
+        <a className="navbar-brand fw-bold" href="/">Book Directory</a>
+      </div>
+    </nav>
+  );
+}
